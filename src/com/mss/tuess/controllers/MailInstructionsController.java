dd51856f004c2044a1b5811cb2514806afd9f1e5
ksquare <karthik.gt90@gmail.com>
2013-03-31 16:49:43 -0700

    mail instructions - email integration

--- src/com/mss/tuess/controllers/MailInstructionsController.java
+++ src/com/mss/tuess/controllers/MailInstructionsController.java
@@ -1,15 +1,26 @@
 package com.mss.tuess.controllers;
 
 import com.mss.tuess.util.ViewManager;
+import com.mss.tuess.util.SendEmail;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javax.mail.MessagingException;
 
 public class MailInstructionsController implements Initializable {
 
-    @FXML Pane sidebar;
+    @FXML 
+    Pane sidebar;
+    
+    @FXML 
+    Button sendInstructions;
+    
+    @FXML
+    Label sendEmailStatus;
     
     /**
      * Initializes the controller class.
@@ -17,5 +28,26 @@ public class MailInstructionsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         ViewManager.loadSidebar(sidebar);
-    }    
+        sendEmailStatus.setText("");
+    }   
+    
+    @FXML
+    private void mailInstructions()throws MessagingException{
+        
+        //These hardcodings are to be removed and replaced by data from the database
+        String[] toEmails = {"karthik.gt90@gmail.com",
+                            "ibmmmm@gmail.com",
+                            "renxin.zju@gmail.com",
+                            "prabalsharma39@gmail.com",
+                            "jasonchenliang@gmail.com",
+                            "wenhcn@gmail.com",
+                            };
+        String emailSubject = "Pre enrolment Activities - TUESS Team";
+        String emailBody = "Test Mail <BR><BR><BR>cheers!!!<BR><b>TUESS Team</b>";
+        //End of hardcoding
+        
+        SendEmail sendEmail = new SendEmail();
+        sendEmail.sendMail(toEmails, emailSubject, emailBody);
+        sendEmailStatus.setText("Mails successfully sent!");
+    }
 }
