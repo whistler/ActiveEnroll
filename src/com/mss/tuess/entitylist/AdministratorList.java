7fdb7d75cccdb8ce7a472e64899b560aad771033
keypointt <iamshrek@126.com>
2013-03-30 16:50:36 -0800

    xin 1650

---
+++ src/com/mss/tuess/entitylist/AdministratorList.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mss.tuess.entitylist;
+
+import com.mss.tuess.entity.Administrator;
+import com.mss.tuess.util.DatabaseConnector;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+/**
+ *
+ * @author x
+ */
+public class AdministratorList {
+    
+    private static ArrayList<Administrator> administrators = new ArrayList();
+
+    /**
+     * Loads all Administrator records from the database in to a list of Administrator
+     * objects
+     *
+     * @throws SQLException
+     */
+    public static void fetch() throws SQLException {
+        ResultSet rs;
+        String sql = "SELECT * FROM administrator";
+        rs = DatabaseConnector.returnQuery(sql);
+        while (rs.next()) {
+            Administrator administrator = new Administrator();
+            administrator.setAdminID(rs.getInt("adminID"));
+            administrator.setFirstName(rs.getString("firstName"));
+            administrator.setLastName(rs.getString("lastName"));
+            administrator.setAddress(rs.getString("address"));
+            administrator.setCity(rs.getString("city"));
+            administrator.setCountry(rs.getString("country"));
+            administrator.setZipcode(rs.getString("zipcode"));
+            administrator.setPhone(rs.getString("phone"));
+            administrator.setPassword(rs.getString("password"));
+
+            administrators.add(administrator);
+        }
+    }
+
+    /**
+     * Returns the administrator stored at the given index
+     *
+     * @param index index of the administrator to return
+     * @return Administrator object at position index
+     */
+    public static Administrator get(int index) {
+        return administrators.get(index);
+    }
+}
