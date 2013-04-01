38ce4591229222ac7dff1657c3eefa421ca994a8
ksquare <karthik.gt90@gmail.com>
2013-04-01 12:38:43 -0700

    course search

--- src/com/mss/tuess/entity/Course.java
+++ src/com/mss/tuess/entity/Course.java
@@ -12,43 +12,43 @@ public class Course {
     private String info;
     private int credit;
 
-    public String getCourseDept() {;
+    public String getCourseDept() {
         return this.courseDept;
     }
 
-    public void setCourseDept(String courseDept) {;
+    public void setCourseDept(String courseDept) {
         this.courseDept = courseDept;
     }
 
-    public String getCourseNum() {;
+    public String getCourseNum() {
         return this.courseNum;
     }
 
-    public void setCourseNum(String courseNum) {;
+    public void setCourseNum(String courseNum) {
         this.courseNum = courseNum;
     }
 
-    public String getCourseName() {;
+    public String getCourseName() {
         return this.courseName;
     }
 
-    public void setCourseName(String courseName) {;
+    public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
-    public String getInfo() {;
+    public String getInfo() {
         return this.info;
     }
 
-    public void setInfo(String info) {;
+    public void setInfo(String info) {
         this.info = info;
     }
 
-    public int getCredit() {;
+    public int getCredit() {
         return this.credit;
     }
 
-    public void setCredit(int credit) {;
+    public void setCredit(int credit) {
         this.credit = credit;
     }
 
