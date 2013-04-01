ae93c533837a3d4468f96f8ff53080464f396e6b
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-04-01 15:14:22 -0700

    Administrator, Instructor and Student properly inherit from User

--- src/com/mss/tuess/entity/Student.java
+++ src/com/mss/tuess/entity/Student.java
@@ -7,17 +7,9 @@ import java.sql.SQLException;
 public class Student extends User {
 
     private int studentID;
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String country;
-    private String zipcode;
-    private String phone;
     private String programID;
     private String registeredSince;
     private String status;
-    private String password;
 
     /**
      * @return the studentID
@@ -32,105 +24,7 @@ public class Student extends User {
     public void setStudentID(int studentID) {
         this.studentID = studentID;
     }
-
-    /**
-     * @return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     * @return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     * @return the address
-     */
-    public String getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the city
-     */
-    public String getCity() {
-        return city;
-    }
-
-    /**
-     * @param city the city to set
-     */
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    /**
-     * @return the country
-     */
-    public String getCountry() {
-        return country;
-    }
-
-    /**
-     * @param country the country to set
-     */
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    /**
-     * @return the zipcode
-     */
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    /**
-     * @param zipcode the zipcode to set
-     */
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    /**
-     * @return the phone
-     */
-    public String getPhone() {
-        return phone;
-    }
-
-    /**
-     * @param phone the phone to set
-     */
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
+    
     /**
      * @return the programID
      */
@@ -174,20 +68,6 @@ public class Student extends User {
     }
 
     /**
-     * @return the password
-     */
-    public String getPassword() {
-        return password;
-    }
-
-    /**
-     * @param password the password to set
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
      * Loads the Student by the studentID from the database and encapsulates
      * into this Student objects
      *
@@ -221,6 +101,7 @@ public class Student extends User {
      *
      * @throws SQLException
      */
+    @Override
     public void update() throws SQLException {
         String sql = "UPDATE student SET firstName=" + this.getFirstName()
                 + ", lastName=" + this.getLastName() + ", address=" + this.getAddress()
@@ -237,6 +118,7 @@ public class Student extends User {
      *
      * @throws SQLException
      */
+    @Override
     public void delete() throws SQLException {
         String sql = "DELETE FROM student WHERE studentID=" + this.getStudentID();
         DatabaseConnector.updateQuery(sql);
@@ -247,6 +129,7 @@ public class Student extends User {
      *
      * @throws SQLException
      */
+    @Override
     public void insert() throws SQLException {
         String sql = "INSERT INTO student  (studentID, firstName, lastName, address, city, country, "
                 + "zipcode, phone, programID, registeredSince, status, password) values "
