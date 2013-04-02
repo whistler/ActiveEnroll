fe89740e1347cba4e074076bdc8153343601d37c
prablsharma <cena_prabal@yahoo.co.in>
2013-04-01 19:24:32 -0700

    User info is shown in Profile

--- src/com/mss/tuess/controllers/ProfileController.java
+++ src/com/mss/tuess/controllers/ProfileController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
@@ -15,23 +16,60 @@ public class ProfileController implements Initializable {
 
     @FXML Pane sidebar;
     
-    @FXML Label studentID;
+    @FXML Label ID;
+    
+    @FXML Label status;
+    @FXML Label program;
+    @FXML Label registeredSince;
+    @FXML Label statusLabel;
+    @FXML Label programLabel;
+    @FXML Label registeredSinceLabel;
+    @FXML Label IDLabel;
+    
     @FXML TextField firstName;
     @FXML TextField lastName;
     @FXML TextField email;
-    @FXML TextField address;
+    @FXML TextArea address;
+    @FXML TextField city;
+    @FXML TextField zipCode;
+    @FXML TextField country;
     @FXML TextField phone;
     @FXML TextField newPassWord;
     @FXML TextField confirmPassWord;
     
     /**
-     * Initializes the controller class.
+     * Initializes the controller class. Shows the current user in the view
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        User user = CurrentUser.getUser();
-        studentID.setText(Integer.toString(user.getID()));
         ViewManager.loadSidebar(sidebar);
-        //firstName.setText("Wen Han");
+        
+        User user = CurrentUser.getUser();
+        ID.setText(Integer.toString(user.getID()));
+        firstName.setText(user.getFirstName());
+        lastName.setText(user.getLastName());
+        email.setText(user.getEmail());
+        address.setText(user.getAddress());
+        city.setText(user.getCity());
+        zipCode.setText(user.getZipcode());
+        country.setText(user.getCountry());
+        phone.setText(user.getPhone());
+ 
+        IDLabel.setText(user.getClass().getSimpleName() + " ID");
+        
+        if(user.getClass()==Student.class)
+        {
+            status.setVisible(true);
+            registeredSince.setVisible(true);
+            program.setVisible(true);
+            statusLabel.setVisible(true);
+            registeredSinceLabel.setVisible(true);
+            programLabel.setVisible(true);
+            
+            Student student = (Student) user;
+            status.setText(student.getStatus());
+            registeredSince.setText(student.getRegisteredSince());
+            program.setText(student.getProgramID());
+        }
     }    
 }
