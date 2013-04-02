8d05b5ebb409da1057ec637921be0281c8e956b4
chen0910 <chen0910@WIN-05116.mss.icics.ubc.ca>
2013-04-02 14:59:57 -0700

    created term entity

---
+++ src/com/mss/tuess/entity/Term.java
@@ -0,0 +1,190 @@
+package com.mss.tuess.entity;
+
+import com.mss.tuess.util.DatabaseConnector;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author chen0910
+ */
+public class Term {
+    private String term;
+    private Date start;
+    private Timestamp registrationStart;
+    private Timestamp registrationEnd;
+    private Timestamp dropWithoutW;
+    private Timestamp dropWithW;
+    private Date end;
+
+    /**
+     * @return the term
+     */
+    public String getTerm() {
+        return term;
+    }
+
+    /**
+     * @param term the term to set
+     */
+    public void setTerm(String term) {
+        this.term = term;
+    }
+
+    /**
+     * @return the start
+     */
+    public Date getStart() {
+        return start;
+    }
+
+    /**
+     * @param start the start to set
+     */
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    /**
+     * @return the registrationStart
+     */
+    public Timestamp getRegistrationStart() {
+        return registrationStart;
+    }
+
+    /**
+     * @param registrationStart the registrationStart to set
+     */
+    public void setRegistrationStart(Timestamp registrationStart) {
+        this.registrationStart = registrationStart;
+    }
+
+    /**
+     * @return the registrationEnd
+     */
+    public Timestamp getRegistrationEnd() {
+        return registrationEnd;
+    }
+
+    /**
+     * @param registrationEnd the registrationEnd to set
+     */
+    public void setRegistrationEnd(Timestamp registrationEnd) {
+        this.registrationEnd = registrationEnd;
+    }
+
+    /**
+     * @return the dropWithoutW
+     */
+    public Timestamp getDropWithoutW() {
+        return dropWithoutW;
+    }
+
+    /**
+     * @param dropWithoutW the dropWithoutW to set
+     */
+    public void setDropWithoutW(Timestamp dropWithoutW) {
+        this.dropWithoutW = dropWithoutW;
+    }
+
+    /**
+     * @return the dropWithW
+     */
+    public Timestamp getDropWithW() {
+        return dropWithW;
+    }
+
+    /**
+     * @param dropWithW the dropWithW to set
+     */
+    public void setDropWithW(Timestamp dropWithW) {
+        this.dropWithW = dropWithW;
+    }
+
+    /**
+     * @return the end
+     */
+    public Date getEnd() {
+        return end;
+    }
+
+    /**
+     * @param end the end to set
+     */
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+    
+    /**
+     * Loads Term with the given term from the database
+     *
+     * @param term term which the course belongs to
+     * @throws SQLException
+     */
+    public void fetch(String term) throws SQLException {
+        String query = "SELECT * FROM term WHERE term = " + term;
+        
+        ResultSet rs;
+        rs = DatabaseConnector.returnQuery(query);
+        
+        if (rs.next()) {
+            this.term = rs.getString("term");
+            this.start = rs.getDate("start");
+            this.registrationStart = rs.getTimestamp("registrationStart");
+            this.registrationEnd = rs.getTimestamp("registrationEnd");
+            this.dropWithoutW = rs.getTimestamp("dropWithoutW");
+            this.dropWithW = rs.getTimestamp("dropWithW");
+            this.end = rs.getDate("end");
+        }
+
+    }
+
+    /**
+     * Updates the current record in the database
+     *
+     * @throws SQLException
+     */
+    public void update() throws SQLException {
+        DatabaseConnector.updateQuery("UPDATE term SET "
+                + "term='" + this.term + "', "
+                + "start='" + this.start + "', "
+                + "registrationStart='" + this.registrationStart + "', "
+                + "registrationEnd='" + this.registrationEnd + "', "
+                + "dropWithoutW='" + this.dropWithoutW + "', "
+                + "dropWithW='" + this.dropWithW + "', "
+                + "end=" + this.end);
+    }
+
+    /**
+     * Deletes the record from the database
+     * @throws SQLException 
+     */
+    public void delete() throws SQLException {
+        DatabaseConnector.updateQuery("DELETE FROM term "
+                + "WHERE term='" + this.term);
+    }
+    
+    /**
+     * Creates a new record with the database with the properties of this
+     * term
+     *
+     * @throws SQLException
+     */
+    public void insert() throws SQLException {
+        String sql = "INSERT INTO term (term, start, registrationStart, registrationEnd, dropWithoutW,dropWithW,end) "
+                + " values ('"
+                + this.term + "', '"
+                + this.start + "', '"
+                + this.registrationStart + "', "
+                + this.registrationEnd + ", '"
+                + this.dropWithoutW + ", '"
+                + this.dropWithW + ", '"
+                + this.end + "')";
+
+        System.out.println(sql);
+        DatabaseConnector.updateQuery(sql);
+    }
+    
+}
