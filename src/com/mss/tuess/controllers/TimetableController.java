8344a3b8f0595da690d128ed490da289e6b698e1
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-04-07 21:34:49 -0700

    Striped repeated parts of the views i.e. header, sidebar & footer. UI layout improvements

---
+++ src/com/mss/tuess/controllers/TimetableController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mss.tuess.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author ibrahim
+ */
+public class TimetableController implements Initializable {
+    @FXML
+    private Label errorLabel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
