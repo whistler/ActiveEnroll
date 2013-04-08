3959cd81c0926088b244ff9dd5502afefa3ba021
wwh <wwh@wwh-PC>
2013-04-07 23:56:54 -0700

    E

--- src/com/mss/tuess/controllers/SectionController.java
+++ src/com/mss/tuess/controllers/SectionController.java
@@ -97,7 +97,8 @@ public class SectionController implements Initializable {
                     + section.getCourseNum() + ", "
                     + section.getTermID()
                     + ")";
-            DatabaseConnector.updateQuery(sql);
+            //DatabaseConnector.updateQuery(sql);
+            System.out.println("\nCan!!!!!!!!   " + sql);
         } else {
             System.out.println("\nCannot be added!!!!!!!!!");
         }
@@ -110,31 +111,80 @@ public class SectionController implements Initializable {
         return rs;
     }
 
-    public static ResultSet fetchPrereqCourses(Course course) throws SQLException {
+    public static ResultSet fetchPrereqCourses(Section section) throws SQLException {
         //CurrentUser.getUser().getID();
         ResultSet rs;
-        String sql = "select prereqNum, prereqDept from prerequisite where courseNum='" +
-                course.getCourseNum() + "' and courseDept='" + course.getCourseDept() + "'"
+        String sql = "select * from prerequisite where courseNum='"
+                + section.getCourseNum() + "' and courseDept='" + section.getCourseDept() + "'"
                 + "ORDER by prereqGroup";
         rs = DatabaseConnector.returnQuery(sql);
         return rs;
     }
 
-    public static ResultSet checkPrerequisite(Section section, int studentID) throws SQLException {
-        ResultSet rs;
-        String sql = " SELECT prereqDept, prereqNum FROM prerequisite pr"
-                + "	WHERE courseDept=" + section.getCourseDept()
-                + "		AND courseNum=" + section.getCourseNum()
-                + "		AND NOT EXISTS"
-                + "			( SELECT * FROM enrollSection e"
-                + "			WHERE  e.studentID=" + studentID
-                + "				AND e.courseDept=pr.prereqDept"
-                + "				AND e.courseNum=pr.prereqNum"
-                + "				AND (e.grade='A' OR e.grade='B' OR e.grade='C' OR e.grade='D')"
-                + "                )";
+    public static boolean isInSet(ResultSet rs_enrolled, String courseDept, String courseNum) throws SQLException {
+        rs_enrolled.beforeFirst();
+        while (rs_enrolled.next()) {
+            if (courseDept.compareTo(rs_enrolled.getString("courseDept")) == 0 && courseNum.compareTo(rs_enrolled.getString("courseNum")) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        rs = DatabaseConnector.returnQuery(sql);
-        return rs;
+    public static boolean checkPrerequisite(Section section, int studentID) throws SQLException {
+//        ResultSet rs;
+//        String sql = " SELECT prereqDept, prereqNum FROM prerequisite pr"
+//                + "	WHERE courseDept=" + section.getCourseDept()
+//                + "		AND courseNum=" + section.getCourseNum()
+//                + "		AND NOT EXISTS"
+//                + "			( SELECT * FROM enrollSection e"
+//                + "			WHERE  e.studentID=" + studentID
+//                + "				AND e.courseDept=pr.prereqDept"
+//                + "				AND e.courseNum=pr.prereqNum"
+//                + "				AND (e.grade='A' OR e.grade='B' OR e.grade='C' OR e.grade='D')"
+//                + "                )";
+//        rs = DatabaseConnector.returnQuery(sql);
+//        return rs;
+        ResultSet rs_enrolled = fetchEnrolledCourses(studentID);
+        ResultSet rs_pre = fetchPrereqCourses(section);
+        System.out.println("+-------------------------------------+");
+        while (rs_enrolled.next()) {
+            System.out.println(rs_enrolled.getString("courseDept") + "__" + rs_enrolled.getString("courseNum"));
+        }
+        System.out.println("=========");
+        while (rs_pre.next()) {
+            System.out.println(rs_pre.getString("prereqDept") + "__" + rs_pre.getString("prereqNum") + "__" + rs_pre.getString("prereqGroup"));
+        }
+        System.out.println("+-------------------------------------+");
+        rs_enrolled.beforeFirst();
+        rs_pre.beforeFirst();
+        int flag = 0;
+        int change = 0;
+        while (rs_pre.next()) {
+            if (change == 0) {
+                flag = 0;
+            }
+            if (isInSet(rs_enrolled, rs_pre.getString("prereqDept"), rs_pre.getString("prereqNum"))) {
+                flag = 1;
+            }
+            String now = rs_pre.getString("prereqGroup");
+            while (!isInSet(rs_enrolled, rs_pre.getString("prereqDept"), rs_pre.getString("prereqNum"))) {
+                rs_pre.next();
+                if (now.compareTo(rs_pre.getString("prereqGroup")) != 0) {
+                    change = 0;
+                    if (flag == 0) {
+                        return false;
+                    }
+                }
+            }
+            flag = 1;
+            change = 1;
+        }
+        if (flag == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static boolean isAlreadyRegistered(Section section, int studentID) throws SQLException {
@@ -144,7 +194,7 @@ public class SectionController implements Initializable {
             if (rs_stu_reg.getString("sectionID").compareTo(section.getSectionID()) == 0
                     && rs_stu_reg.getString("courseDept").compareTo(section.getCourseDept()) == 0
                     && rs_stu_reg.getString("courseNum").compareTo(section.getCourseNum()) == 0) {
-        System.out.println("\nisAlreadyRegistered returns: true");
+                System.out.println("\nisAlreadyRegistered returns: true");
                 return true;
             }
         }
@@ -153,18 +203,18 @@ public class SectionController implements Initializable {
     }
 
     public static boolean canEnroll(Section section, int studentID) throws SQLException {
-        if (!registrationEndNotPass(section)) {
-            return false;
-        }
-        if (isAlreadyRegistered(section, studentID)) {
-            return false;
-        }
-        if (notFull(section)) {
-            return false;
-
-        }
-        if (checkPrerequisite(section, studentID).getRow() == 0) {
-        System.out.println("\ncheckPrerequisite: false");
+//        if (!registrationEndNotPass(section)) {
+//            return false;
+//        }
+//        if (isAlreadyRegistered(section, studentID)) {
+//            return false;
+//        }
+//        if (notFull(section)) {
+//            return false;
+//
+//        }
+        if (checkPrerequisite(section, studentID) == false) {
+            System.out.println("\ncheckPrerequisite: false");
             return false;
         }
         System.out.println("\ncheckPrerequisite: true");
@@ -174,22 +224,22 @@ public class SectionController implements Initializable {
     public static boolean registrationEndNotPass(Section section) {
         Term currentTerm = State.getCurrentTerm();
         Timestamp now = new Timestamp(System.currentTimeMillis());
+        System.out.println(now + "___" + currentTerm.getRegistrationEnd());
         if (now.compareTo(currentTerm.getRegistrationEnd()) < 0) {
-        System.out.println(now+"___"+currentTerm.getRegistrationEnd());
-        System.out.println("\nregistrationEndNotPass returns: true");
+            System.out.println("\nregistrationEndNotPass returns: true");
             return true;
         } else {
-        System.out.println("\nregistrationEndNotPass returns: false");
+            System.out.println("\nregistrationEndNotPass returns: false");
             return false;
         }
     }
 
     public static boolean notFull(Section section) {
         if (section.getStatus().compareTo("open") == 0) {
-        System.out.println("\nnotFull returns: true");
+            System.out.println("\nnotFull returns: true");
             return true;
         } else {
-        System.out.println("\nnotFull returns: false");
+            System.out.println("\nnotFull returns: false");
             return false;
         }
     }
