c269fa326d0913e1e6b322ef50f691d0544f14b6
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2013-04-10 23:20:02 -0700

    section view displays schedule

--- src/com/mss/tuess/controllers/SearchCoursesController.java
+++ src/com/mss/tuess/controllers/SearchCoursesController.java
@@ -22,8 +22,6 @@ import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
