ce1267646c969b0dddce9aa0b542593f4a389f89
ksquare <karthik.gt90@gmail.com>
2013-04-20 19:41:34 -0700

    Mail Grades - Performance tuned!

--- src/com/mss/tuess/controllers/MailGradesController.java
+++ src/com/mss/tuess/controllers/MailGradesController.java
@@ -1,15 +1,16 @@
 package com.mss.tuess.controllers;
 
 import com.mss.tuess.entity.Student;
-import com.mss.tuess.entity.Transcriptrecord;
-import com.mss.tuess.util.CurrentUser;
 import com.mss.tuess.util.DatabaseConnector;
+import com.mss.tuess.util.EmailData;
 import com.mss.tuess.util.SendEmail;
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -40,11 +41,16 @@ public class MailGradesController implements Initializable {
 
     @FXML
     private void mailInstructions() throws MessagingException, InterruptedException, SQLException {
-
+        
+        ArrayList<EmailData> emailList;
+        emailList = new ArrayList<>();
+        EmailData emailData;
+        
         mailProgressLabel.setText("Please wait. Mailing Grades...");
         mailGradeProgress.setVisible(true);
         mailGradeProgress.setProgress(-0.59F);
-        int studentnumber=0;
+        int studentnumber = 0;
+        
         ResultSet mailrs;
 
         mailrs = Student.fetchCurrentTerm();
@@ -64,9 +70,9 @@ public class MailGradesController implements Initializable {
 
 
             rs.first();
-            //These hardcodings are to be removed and replaced by data from the database
+            
             String toEmails = rs.getString("email");
-            String emailSubject = "Term Grades - TUESS Team";
+            String emailSubject = "Term Grades for student ID: "+ currentID +"- TUESS Team";
             //System.out.println("into_student_loop_resultset_after_passemail");
             String emailBody1 = "<!DOCTYPE html>\n"
                     + "<html>\n"
@@ -83,9 +89,6 @@ public class MailGradesController implements Initializable {
             String emailBody2 = "\n";
             rs.beforeFirst();
 
-
-
-
             while (rs.next()) {
                 String mailCourseNumber = rs.getString("coursenum");
                 String mailCourseName = rs.getString("coursename");
@@ -99,8 +102,6 @@ public class MailGradesController implements Initializable {
                         "    <th>" + mailGrade + "</th>\n" + // !!!!!!!!
                         "  </tr>\n"
                         + "  <tr>\n";
-
-
             }
 
             String emailBody3 = "  </tr>\n"
@@ -113,30 +114,33 @@ public class MailGradesController implements Initializable {
                     + "\n"
                     + "</body>\n"
                     + "</html>";
-            //End of hardcoding
-            //System.out.println(emailBody3);
-            //System.out.println("outof_addcourse_loop");
+
             
             String emailBody = emailBody1 + emailBody2 + emailBody3;
-            //System.out.println(toEmails);
-            SendEmail sendEmail = new SendEmail();
-            sendEmail.sendMail(toEmails, emailSubject, emailBody);
             
-            studentnumber++;
-            System.out.println(studentnumber+"th email is "+toEmails);
+            //build email list
+            emailData = new EmailData(toEmails, emailSubject, emailBody);
+            System.out.println("Adding student "+studentnumber);
+            emailList.add(emailData);
 
-
-
-
-
-
-
-
-
-            mailGradesStatus.setText("Grades successfully mailed!");
-            mailProgressLabel.setText("");
-            mailGradeProgress.setProgress(1.00F);
+            //mailGradesStatus.setText("Grades successfully mailed!");
+            //mailProgressLabel.setText("");
+            //mailGradeProgress.setProgress(1.00F);
+        }
+        
+        /**
+         * Send Email
+         */
+        try {
+            if(emailList.size() <= 0){
+            } else {
+                SendEmail sendMail = new SendEmail();
+                sendMail.sendMail(emailList);
+                mailProgressLabel.setText("Grades mailed succesfully");
+                mailGradeProgress.setProgress(1.00F);
+            }
+        } catch (MessagingException ex) {
+            Logger.getLogger(com.mss.tuess.controllers.MailGradesController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-; // while loop for each student ends here
 }
