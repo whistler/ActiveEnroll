63b2010f9c6ac456178126d6015b77314ba8ce50
Wenhan Wu <wenhcn@gmail.com>
2013-04-21 12:11:26 -0700

    Modified taoughtcourse

--- src/com/mss/tuess/controllers/TaughtCourseController.java
+++ src/com/mss/tuess/controllers/TaughtCourseController.java
@@ -49,7 +49,14 @@ public class TaughtCourseController implements Initializable {
     @FXML
     private TableColumn<EnrollSection, String> sectionID;
     @FXML
+    private TableColumn<EnrollSection, String> studentID;
+    @FXML
     private TableColumn<EnrollSection, String> grade;
+    //filter fields
+    @FXML
+    private TextField sectionFilter;
+    @FXML
+    private TextField studentFilter;
     //Section list
     private ObservableList<Section> sectionTableContent = FXCollections.observableArrayList();
     private ObservableList<Section> sectionFilterContent = FXCollections.observableArrayList();
@@ -77,6 +84,111 @@ public class TaughtCourseController implements Initializable {
         }
         sectionFilterContent.addAll(sectionTableContent);
 
+        //event listener for course filter 
+        sectionTableContent.addListener(new ListChangeListener<Section>() {
+            @Override
+            public void onChanged(ListChangeListener.Change<? extends Section> change) {
+                sectionFilterRefresh();
+            }
+        });
+
+        //event listener for course filter 
+        studentTableContent.addListener(new ListChangeListener<EnrollSection>() {
+            @Override
+            public void onChanged(ListChangeListener.Change<? extends EnrollSection> change) {
+                enrollSectionFilterRefresh();
+            }
+        });
+    }
+
+    /**
+     * Handle the order for each course column
+     */
+    private void tableOrderAct() {
+        ArrayList<TableColumn<Section, ?>> sortOrder = new ArrayList(sectionTable.getSortOrder());
+        sectionTable.getSortOrder().clear();
+        sectionTable.getSortOrder().addAll(sortOrder);
+    }
+
+    /**
+     * Handle the order for each section column
+     */
+    private void sectionClassTableOrderAct() {
+        ArrayList<TableColumn<EnrollSection, ?>> sectionSortOrder = new ArrayList(studentTable.getSortOrder());
+        studentTable.getSortOrder().clear();
+        studentTable.getSortOrder().addAll(sectionSortOrder);
+    }
+
+    /**
+     * Refresh the content of the filter
+     */
+    private void sectionFilterRefresh() {
+        sectionFilterContent.clear();
+        for (Section section : sectionTableContent) {
+            if (sectionFilterChecker(section)) {
+                sectionFilterContent.add(section);
+            }
+        }
+        tableOrderAct();
+    }
+
+    /**
+     * Refresh the content of the filter
+     */
+    private void enrollSectionFilterRefresh() {
+        studentFilterContent.clear();
+        for (EnrollSection enrollsection : studentTableContent) {
+            if (enrollSectionFilterChecker(enrollsection)) {
+                studentFilterContent.add(enrollsection);
+            }
+        }
+        sectionClassTableOrderAct();
+    }
+
+    /**
+     * Control the course filter
+     *
+     * @param section the course object
+     */
+    private boolean sectionFilterChecker(Section section) {
+        String filterString = sectionFilter.getText();
+        if (filterString == null || filterString.isEmpty()) {
+            return true;
+        }
+
+        String lowerCaseFilterString = filterString.toLowerCase();
+
+        if (section.getSectionID().toLowerCase().indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        } else if (section.getCourseDept().toLowerCase().indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        } else if (section.getCourseNum().toLowerCase().indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        } else if (section.getTermID().toLowerCase().indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Control the section class filter
+     *
+     * @param SectionClass the course object
+     */
+    private boolean enrollSectionFilterChecker(EnrollSection enrollsection) {
+        String filterString = sectionFilter.getText();
+        if (filterString == null || filterString.isEmpty()) {
+            return true;
+        }
+
+        String lowerCaseFilterString = filterString.toLowerCase();
+
+        if (Integer.toString(enrollsection.getStudentID()).indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        } else if (enrollsection.getGrade().toLowerCase().indexOf(lowerCaseFilterString) != -1) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -95,7 +207,7 @@ public class TaughtCourseController implements Initializable {
         term.setCellValueFactory(new PropertyValueFactory<Section, String>("term"));
         credit.setCellValueFactory(new PropertyValueFactory<Section, String>("credit"));
 
-//        sectionTable.setItems(sectionFilterContent);
+        sectionTable.setItems(sectionFilterContent);
 
         /*
          * Event Handler to capture the selected course
@@ -132,13 +244,20 @@ public class TaughtCourseController implements Initializable {
             @Override
             public void changed(ObservableValue<? extends EnrollSection> ov, EnrollSection t, EnrollSection t1) {
                 int selectedIndex = studentTable.getSelectionModel().getSelectedIndex();
-                Section currentSection = new Section();
+                EnrollSection currentEnrollSection = new EnrollSection();
 
                 System.out.println("Index : " + selectedIndex);
 
                 try {
-
-                    ViewManager.changeView("/com/mss/tuess/views/Section.fxml");
+                    State.setCurrentEnrollSection(studentFilterContent.get(selectedIndex));
+                    currentEnrollSection.fetch(studentFilterContent.get(selectedIndex).getStudentID(),studentFilterContent.get(selectedIndex).getSectionID(),
+                            studentFilterContent.get(selectedIndex).getCourseDept(),
+                            studentFilterContent.get(selectedIndex).getCourseNum(),
+                            studentFilterContent.get(selectedIndex).getTermID());
+                    State.setCurrentEnrollSection(currentEnrollSection);
+                    System.out.println(currentEnrollSection.getStudentID());
+                    
+                    
                 } catch (Exception ex) {
                     Logger.getLogger(SearchCoursesController.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -150,6 +269,9 @@ public class TaughtCourseController implements Initializable {
          * map the section table attributes
          */
         sectionID.setCellValueFactory(new PropertyValueFactory<EnrollSection, String>("sectionID"));
+        studentID.setCellValueFactory(new PropertyValueFactory<EnrollSection, String>("studentID"));
+
+        grade.setCellValueFactory(new PropertyValueFactory<EnrollSection, String>("grade"));
 
         studentTable.setItems(studentFilterContent);
     }
